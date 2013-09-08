4b18b9a77e5cb251098920275f7b656f3378a7a6
Luis Thiago <luisthiago.ti@gmail.com>
2013-09-08 13:35:20 -0300

    Classe MetodosSobrecarregados adicionada

--- src/br/com/estudo/exercicios/MetodosSobrecarregados.java
+++ src/br/com/estudo/exercicios/MetodosSobrecarregados.java
@@ -1,5 +1,38 @@
 package br.com.estudo.exercicios;
 
 public class MetodosSobrecarregados {
+	
+	int soma;
+	
+	public void soma() {
+		System.out.println ("Atenção: \n");
+	}
+	
+	public void soma(int a, int b, int c) {
+		
+		this.soma = a + b + c;
+		System.out.println("int a, int b, int c");
+		System.out.println("Resultado; "+this.soma);
+	}
+	
+	public void soma(int a, int b) {
+		
+		this.soma = a + b;
+		System.out.println("int a, int b");
+		System.out.println("Resultado; "+this.soma);
+
+	}
+	
+	public void soma (int ... numero) {
+		
+		for (int i=0;i<numero.length;i++) {
+			this.soma = this.soma + numero[i];
+			i ++;
+		}
+		
+		System.out.println("String ... numero");
+		System.out.println("Resultado: "+ this.soma);
+
+	}
 
 }
--- src/br/com/estudo/exercicios/MetodosSobrecarregadosMain.java
+++ src/br/com/estudo/exercicios/MetodosSobrecarregadosMain.java
@@ -4,7 +4,12 @@ public class MetodosSobrecarregadosMain {
 	
 	public static void main(String[] args) {
 		
-				
+		MetodosSobrecarregados c = new MetodosSobrecarregados();
+		
+		c.soma(1, 2);
+		c.soma(1, 2, 3);
+		c.soma(1, 2, 3, 4);
+		c.soma();
 	}
 
 }
