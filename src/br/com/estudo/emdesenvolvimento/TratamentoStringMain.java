6977358c59573d6fa16f5b88325d764769f7f3c0
Luis Thiago <luisthiago.ti@gmail.com>
2013-09-08 12:22:37 -0300

    Novo exercicio

---
+++ src/br/com/estudo/emdesenvolvimento/TratamentoStringMain.java
@@ -0,0 +1,21 @@
+/*	
+    Write a java program to print the following pattern:
+	
+	A
+	BC
+	DEF
+	GHIJ
+	KLMNO
+	
+*/
+
+package br.com.estudo.emdesenvolvimento;
+
+public class TratamentoStringMain {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
