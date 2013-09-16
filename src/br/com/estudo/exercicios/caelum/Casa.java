2eb18872f24798138c833d4dd685fa8175fe9678
Luis Thiago <luisthiago.ti@gmail.com>
2013-09-16 14:14:31 -0300

    Classe Casa - Portas

--- src/br/com/estudo/exercicios/caelum/Casa.java
+++ src/br/com/estudo/exercicios/caelum/Casa.java
@@ -3,32 +3,42 @@ package br.com.estudo.exercicios.caelum;
 public class Casa {
 	
 	String cor,
-		   totalDePortas,
 		   portas[];
-	int abertas;
-	
-	Porta p;
+	int abertas=0,
+		totalDePortas=0;
 	
 	void pinta(String s) {
 		System.out.println("Pintei de "+s);
 	}
 	
-	/*int quantasPortasEstaoAbertas() {
-		//abertas = 
+	int quantasPortasEstaoAbertas() {
 		return abertas;
-	}*/
-	
-	void adicionaPorta (Porta p) {
-		
-		
 	}
 	
-	/*int totalDePortas () {
-		
-	}*/
+	void adicionaPorta (Porta p) {
+		totalDePortas++;
+		if(p.aberta==true) {
+			//System.out.println("porta aberta");
+			abertas++;
+		} else {
+			//System.out.println("porta fechada");
+		}
+	}
+	
+	int totalDePortas () {
+		return totalDePortas;
+	}
 }
 
 class Porta {
-	String material,
-		   situacao;
+		boolean aberta;
+	
+	/*void estaAberta() {
+		if(aberta == true) {
+			System.out.println("Porta aberta!");
+		}else {
+			System.out.println("porta fechada!");
+		}
+	}*/
+		
 }
--- src/br/com/estudo/exercicios/caelum/CasaMain.java
+++ src/br/com/estudo/exercicios/caelum/CasaMain.java
@@ -5,10 +5,21 @@ public class CasaMain {
 	public static void main(String[] args) {
 
 		Casa casa = new Casa();
+		Porta p1 = new Porta();
+		Porta p2 = new Porta();
+		Porta p3 = new Porta();
 		
+		p1.aberta = true;
+
 		casa.pinta("Azul");
 		
-		casa.adicionaPorta();
+		casa.adicionaPorta(p1);
+		casa.adicionaPorta(p2);
+		casa.adicionaPorta(p3);
+		
+		System.out.println("Portas abertas: "+casa.quantasPortasEstaoAbertas());
+		System.out.println("Total de portas: "+casa.totalDePortas());
+
 	}
 
 }
