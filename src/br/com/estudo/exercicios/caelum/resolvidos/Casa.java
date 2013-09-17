ea02cd9a53bd3bc6d9f899d9b9c4214c0663d2fe
Luis Thiago <luisthiago.ti@gmail.com>
2013-09-17 14:34:58 -0300

    Classe casa - INCOMPLETO

---
+++ src/br/com/estudo/exercicios/caelum/resolvidos/Casa.java
@@ -0,0 +1,21 @@
+package br.com.estudo.exercicios.caelum.resolvidos;
+
+public class Casa {
+
+	String cor,
+		   porta1,
+		   porta2,
+		   porta3;
+	int abertas;
+	
+	void pinta(String s) {
+		//cor=
+	}
+	
+	int quantasPortasEstaoAbertas() {
+		
+		return abertas;
+	}
+	
+	
+}
---
+++ src/br/com/estudo/exercicios/caelum/resolvidos/CasaMain.java
@@ -0,0 +1,16 @@
+package br.com.estudo.exercicios.caelum.resolvidos;
+
+public class CasaMain {
+
+	public static void main(String[] args) {
+
+		Porta p1 = new Porta();
+		Casa casa= new Casa();		
+
+		casa.cor="verde";
+		casa.porta1="porta 1";
+		casa.porta2="porta 2";
+		casa.porta3="porta 3";
+	}
+
+}
