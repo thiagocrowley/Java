81abc92830ad0538c20eee71e4bd17e126723938
Luis Thiago <luisthiago.ti@gmail.com>
2013-09-17 13:15:19 -0300

    Classe pessoa - Exercício Caelum

---
+++ src/br/com/estudo/exercicios/caelum/Pessoa.java
@@ -0,0 +1,12 @@
+package br.com.estudo.exercicios.caelum;
+
+public class Pessoa {
+
+	String nome;
+	int idade;
+	
+	void fazAniversario() {
+		idade=idade+1;
+		System.out.println("Idade atual: "+idade);
+	}
+}
---
+++ src/br/com/estudo/exercicios/caelum/PessoaMain.java
@@ -0,0 +1,19 @@
+package br.com.estudo.exercicios.caelum;
+
+import javax.swing.JOptionPane;
+
+public class PessoaMain {
+
+	public static void main(String[] args) {
+		
+		Pessoa p = new Pessoa();
+		
+		p.nome=JOptionPane.showInputDialog(null, "Digite seu nome: ");
+		p.idade=Integer.parseInt(JOptionPane.showInputDialog(null, "digite sua idade: "));
+		p.fazAniversario();
+		p.fazAniversario();
+		p.fazAniversario();
+
+	}
+
+}
