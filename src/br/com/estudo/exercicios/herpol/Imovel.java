b8b0bc7ff0b04e5501e90a5d725fe9368db32365
Luis Thiago <luisthiago.ti@gmail.com>
2013-09-20 18:12:07 -0300

    INCOMPLETO - Exercício herpol

---
+++ src/br/com/estudo/exercicios/herpol/Imovel.java
@@ -0,0 +1,14 @@
+package br.com.estudo.exercicios.herpol;
+
+public class Imovel {
+	
+	String endereco;
+	double preco;
+	
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+}
---
+++ src/br/com/estudo/exercicios/herpol/Novo.java
@@ -0,0 +1,6 @@
+package br.com.estudo.exercicios.herpol;
+
+public class Novo extends Imovel {
+
+	
+}
