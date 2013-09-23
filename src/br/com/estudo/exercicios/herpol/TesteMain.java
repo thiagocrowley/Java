43070ad9e4e7fa6a97217ce018d74be2f043bc9d
Luis Thiago <luisthiago.ti@gmail.com>
2013-09-23 18:15:31 -0300

    Classe Teste - HERPOL INCOMPLETA

---
+++ src/br/com/estudo/exercicios/herpol/TesteMain.java
@@ -0,0 +1,16 @@
+package br.com.estudo.exercicios.herpol;
+/*package br.com.estudo.exercicios.herpol.animal;
+package br.com.estudo.exercicios.herpol.funcionarios;
+package br.com.estudo.exercicios.herpol.imovel;
+package br.com.estudo.exercicios.herpol.ingressos;*/
+
+
+
+public class TesteMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
