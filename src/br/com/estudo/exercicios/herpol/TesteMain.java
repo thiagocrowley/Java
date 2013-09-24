2be8c44928702b1ed96938ec9f1723fc3cad9019
Luis Thiago <luisthiago.ti@gmail.com>
2013-09-24 09:00:12 -0300

    Teste com método principal para testar todos os exercícios.

--- src/br/com/estudo/exercicios/herpol/TesteMain.java
+++ src/br/com/estudo/exercicios/herpol/TesteMain.java
@@ -5,12 +5,38 @@ package br.com.estudo.exercicios.herpol.imovel;
 package br.com.estudo.exercicios.herpol.ingressos;
 */
 
+import br.com.estudo.exercicios.herpol.animal.*;
+import br.com.estudo.exercicios.herpol.funcionarios.*;
+import br.com.estudo.exercicios.herpol.imovel.*;
+import br.com.estudo.exercicios.herpol.ingresso.*;
+import br.com.estudo.exercicios.herpol.pessoa.*;
+
 
 public class TesteMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Administrativos adm = new Administrativos();
+		Tecnicos tec = new Tecnicos();
+		Cachorro dog = new Cachorro();
+		Gato cat = new Gato();
+		
+		
+		adm.setMatricula("123");
+		adm.setNome("Luis");
+		System.out.println("Assistente administrativo"
+						    + "\nNome: "+adm.getNome()
+						    + "\nMatrícula: "+adm.getMatricula());
+		
+		tec.setMatricula("456");
+		tec.setNome("Thiago");
+		System.out.println("\nAssistente tecnico"
+						    + "\nNome: "+tec.getNome()
+						    + "\nMatrícula: "+tec.getMatricula());
+		
+		System.out.println();
+		dog.late();
+		cat.mia();
 	}
 
 }
