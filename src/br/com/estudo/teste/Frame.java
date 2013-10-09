47a0e0346a12ae0c333db43dabe70de9f2ecebea
Luis Thiago <luisthiago.ti@gmail.com>
2013-10-09 17:14:27 -0300

    Testando Interface Gráfica

---
+++ src/br/com/estudo/teste/Frame.java
@@ -0,0 +1,15 @@
+package br.com.estudo.teste;
+import javax.swing.*;
+
+public class Frame {
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame();
+		frame.setSize(200, 200);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.getdwa
+	}
+
+}
