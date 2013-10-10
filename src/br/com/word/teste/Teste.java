37b9c509c3bcd1150fa24980f2a1cf4d8d563ee7
Luis Thiago <luisthiago.ti@gmail.com>
2013-10-10 17:59:11 -0300

    Classe Teste separada

--- src/br/com/word/teste/Teste.java
+++ src/br/com/word/teste/Teste.java
@@ -9,38 +9,65 @@ public class Teste {
 	public static void main(String[] args) throws IOException {
 
 		String conteudo, 
-			   cpf;
-		int cont=0;
-		
+		cpf="",
+		autor="",
+		endereco="";
+		//		int cont=0;
+
 		PdfParser pdf = new PdfParser();
-		
+
 		pdf.setEnderecoRecurso("C:/ini.pdf");
 		conteudo = pdf.getConteudo();
-		
+
 		FileWriter arq = new FileWriter("c:/thiago/testeasdf.txt");
 		PrintWriter grava = new PrintWriter(arq);
+
+		//gravar teste autor.txt
+		FileWriter aut = new FileWriter("c:/thiago/autor.txt");
+		PrintWriter grvautor = new PrintWriter(aut);
+
+		Captura cap = new Captura();
 		
-		for(int i = 0; i<conteudo.length();i++) {
-			
-			if (conteudo.charAt(i) == ';'){
-//				grava.print(conteudo.charAt(i));
-				cont++;
-			}
+		autor = cap.capturaAutor(conteudo);
+		cpf = cap.capturaCPF(conteudo);
 		
-			/*while (conteudo.charAt(i) == ';'){
-				grava.print(conteudo.charAt(i));
-				cont++;
-			}*/
-		}
-		
-		System.out.println(cont);
-		
-		grava.print(conteudo);
+		System.out.println("autor: "+autor);
+		System.out.println("cpf: "+cpf);
+
+		/*for(int i = 0; i<conteudo.length();i++) {
+			if (conteudo.charAt(i) == ')'){
+
+				if (conteudo.charAt(i) == ')'){
+					do{
+						autor = autor+conteudo.charAt(i);
+						i++;
+					}while(conteudo.charAt(i) != ',');
+					
+				}else if (conteudo.charAt(i) == '.'){
+					do{
+						cpf = cpf+conteudo.charAt(i);
+						i++;
+					}while(conteudo.charAt(i) != ',');
+					
+				} else {
+					i++;
+					System.out.println("ultimo else");
+				}
+				System.out.println("sai");
+			}			
+		}*/
+
+		/*System.out.print("Autor: "+autor);
+		System.out.print("\nCPF: "+cpf);
+
+		grvautor.print(autor);
+		grvautor.print(cpf);
+		grvautor.print(endereco);
+		grvautor.close();*/
+
+		/*grava.print(conteudo);
 		grava.close();
-		
-//		System.out.println("Autor: "+autor);
-//		System.out.println("CPF: "+cpf);
-//		System.out.println("");
+		 */
 	}
 
 }
