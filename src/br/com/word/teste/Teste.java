8036f3ea51c965b9a0c36982f39cf99e42101a74
Luis Thiago <luisthiago.ti@gmail.com>
2013-10-15 14:55:10 -0300

    linhas comentadas

--- src/br/com/word/teste/Teste.java
+++ src/br/com/word/teste/Teste.java
@@ -1,5 +1,6 @@
 package br.com.word.teste;
 
+import java.awt.List;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,52 +10,56 @@ public class Teste {
 	public static void main(String[] args) throws IOException {
 
 		String conteudo, 
-		cpf="",
-		autor="",
-		endereco="";
-				int cont=0;
-
+				cpf="",
+				autor="",
+				endereco="";
+		int cont=0;
+		
 		PdfParser pdf = new PdfParser();
+		Captura cap = new Captura();
 
-		pdf.setEnderecoRecurso("C:/ini.pdf");
-		conteudo = pdf.getConteudo();
+		pdf.setEnderecoRecurso("C:/ini.pdf");//arquivo a ser desmembrado
+		conteudo = pdf.getConteudo();//Salva conteúdo do arquivo na variável "conteudo"
 
-		FileWriter arq = new FileWriter("c:/thiago/testeasdf.txt");
+		//Cria e grava o arquivo
+		FileWriter arq = new FileWriter("c:/thiago/testeasdf.txt"); 
 		PrintWriter grava = new PrintWriter(arq);
 
 		//gravar teste autor.txt
 		FileWriter aut = new FileWriter("c:/thiago/autor.txt");
 		PrintWriter grvautor = new PrintWriter(aut);
-		
+
 		//Conta a quantidade de autores
 		for (int i=0 ; i<conteudo.length();i++) {
 			if(conteudo.charAt(i) == ';') {
 				cont++;
 			} else{
-				
+
 			}
 		}
-
+		//mostra na tela a quantidade de autor
 		System.out.println("Quantidade de autores: "+cont);
-		
-		Captura cap = new Captura();
-		
+
+		//variaveis recebem o valor capturado
 		autor = cap.capturaAutor(conteudo);
 		cpf = cap.capturaCPF(conteudo);
 		endereco = cap.capturaEndereco(conteudo);
 
+		//apresenta os dados capturados na tela
 		System.out.print("Autor: "+autor);
 		System.out.print("\nCPF: "+cpf);
 		System.out.println("\nEndereço: "+endereco);
 
+		//grava no arquivo autor os dados coletados
 		grvautor.print(autor);
 		grvautor.print(cpf);
 		grvautor.print(endereco);
 		grvautor.close();
 
+		//grava no arquivo principal todo o conteudo para txt
 		grava.print(conteudo);
 		grava.close();
-		 
+		
 	}
 
 }
