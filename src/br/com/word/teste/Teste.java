be942ccf6cda2aa8395f32f7019dd72ea72b12ad
Luis Thiago <luisthiago.ti@gmail.com>
2013-10-16 10:32:31 -0300

    Ajustes para captura de endereço

--- src/br/com/word/teste/Teste.java
+++ src/br/com/word/teste/Teste.java
@@ -1,6 +1,5 @@
 /**
  * 
- *  * A primeira autora não está aparecendo;
  *  * Endereço não está sendo informado;
  *  
  **/
@@ -17,7 +16,7 @@ public class Teste {
 
 		String conteudo;
 		int cont=0;
-		
+
 		PdfParser pdf = new PdfParser();
 		Captura cap = new Captura();
 
@@ -26,7 +25,7 @@ public class Teste {
 		conteudo = conteudo.replaceAll("[\\)]", ";");//transforma parentese em ponto e vírgula
 		conteudo = conteudo.replaceAll("\\s", " ");//codigo para transformar quebra de linha em espaço
 		conteudo = conteudo.replaceAll("  ", " ");//transforma espaço duplo em espaço unico
-		
+
 		//Cria e grava o arquivo
 		FileWriter arq = new FileWriter("c:/thiago/testeasdf.txt"); 
 		PrintWriter grava = new PrintWriter(arq);
@@ -34,18 +33,18 @@ public class Teste {
 		//gravar teste autor.txt
 		FileWriter aut = new FileWriter("c:/thiago/autor.txt");
 		PrintWriter grvautor = new PrintWriter(aut);
-		
+
 		//Conta a quantidade de autores
 		for (int i=0 ; i<conteudo.length();i++) {
 			if(conteudo.charAt(i) == ';') {
 				cont++;
 			} else{
-//				System.out.println("FAVOR IMPLEMENTAR O ELSE QUANTIDADE DE AUTORES");
+				//				System.out.println("FAVOR IMPLEMENTAR O ELSE QUANTIDADE DE AUTORES");
 			}
 		}
-		
+
 		System.out.println("Quantidade de autores: "+cont);
-		
+
 		//identifica posição do ";"
 		int[] posicao = new int[cont];
 		String[] autor = new String[cont];
@@ -57,10 +56,10 @@ public class Teste {
 				posicao[n]=i;
 				n++;
 			} else{
-//				System.out.println("FAVOR IMPLEMENTAR O ELSE QUANTIDADE DE AUTORES");
+				//				System.out.println("FAVOR IMPLEMENTAR O ELSE QUANTIDADE DE AUTORES");
 			}
 		}
-		
+
 		//atribui autor
 		for (int i=0 ; i<posicao.length;i++) {
 			autor[i] = cap.capturaAutor(conteudo, posicao[i]);
@@ -70,23 +69,23 @@ public class Teste {
 
 		//apresenta os dados capturados na tela
 		for (int i=0 ; i<posicao.length;i++) {
-			System.out.println(i+1+"º cadastro");
+		/*	System.out.println(i+1+"º cadastro");
 			System.out.println(" autor: "+autor[i]);
 			System.out.println(" cpf: "+cpf[i]);
-			System.out.println(" endereco: "+endereco[i]+"\n");
-			
+			System.out.println(" endereco: "+endereco[i]+"\n");*/
+
 			grvautor.println("Autor: "+autor[i]);
 			grvautor.println("CPF: "+cpf[i]);
 			grvautor.println("Endereço: "+endereco[i]+"\n");
 			grvautor.println("");
-			
+
 		}
 		grvautor.close();
 
 		//grava no arquivo principal todo o conteudo para txt
 		grava.print(conteudo);
 		grava.close();
-		
+
 	}
 
 }
